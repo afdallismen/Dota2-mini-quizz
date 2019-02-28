bd9a3e2d732e0079adb4cfec1e080a5f308e9cca
afdallismen <afdal.lismen@gmail.com>
2019-02-28 23:21:06 +0700

    Change heroes data file from json into javascript. Can load random heroes voice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
 function GuessDota2Hero () {
-    return {
-        hero: {
-            id: '',
-            name: ''
-        }
-    }
+    return heroes[Math.floor(Math.random() * heroes.length)]
 }
 
+window.onload = function () {
+    var hero = new GuessDota2Hero()
+    
+    if (hero) {
+        var path = "assets/audio/" + hero.name + "/" + hero.voiceFilenames[0]
+        var audio = document.getElementsByTagName("audio")[0]
+        audio.firstElementChild.setAttribute("src", path)
+        audio.load()
+    }
+}
