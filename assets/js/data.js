109138db5f68462c3f9500ed69dd28d5cb45bf4e
afdallismen <afdal.lismen@gmail.com>
2019-03-02 23:55:40 +0700

    Finished with styling. Basic function working

---
+++ assets/js/data.js
@@ -0,0 +1,42 @@
+var data = { 
+    heroes: [{
+        "id": "0",
+        "name": "Abaddon",
+        "genre": "male",
+        "voices": [{
+                "filename": "Abad_attack_08.mp3",
+                "subtitle": "By the House Avernus."
+            },
+            {
+                "filename": "Abad_levelup_06.mp3",
+                "substitle": "Look upon the Lord of Avernus and beware."
+            }
+        ]
+    },
+    {
+        "id": "1",
+        "name": "Alchemist",
+        "genre": "male",
+        "voices": [{
+            "filename": "Alch_move_20.mp3",
+            "subtitle": "Easy now, this stuff is explosive!"
+        },
+        {
+            "filename": "Alch_spawn_05.mp3",
+            "subtitle": "I'm the brains! And I'm the the brawn!"
+        }]
+    },
+    {
+        "id": "2",
+        "name": "Ancient Apparation",
+        "genre": "male",
+        "voices": [{
+            "filename": "Appa_ability_touch_01.mp3",
+            "subtitle": "Feel the chill!"
+        },
+        {
+            "filename": "Appa_spawn_03.mp3",
+            "subtitle": "A cold wind blows."
+        }]
+    }]
+}
